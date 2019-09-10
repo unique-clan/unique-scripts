32e9935899be996bd938bdd19e705de8d7c8d0ff
Tim Schumacher <tim@timakro.de>
2019-09-10 14:51:06 +0200

    Keep just one DB backup per month and 10 at max

--- sql/database_backup.py
+++ sql/database_backup.py
@@ -2,6 +2,7 @@
 import os
 import subprocess
 import datetime
+from glob import glob
 
 import tw
 
@@ -15,6 +16,21 @@ def backup(prefix, table_types):
     cmd = 'mysqldump records {} > {}'.format(tables, path)
     print("Creating backup of {} from {}".format(prefix, datestr))
     subprocess.run(cmd, shell=True)
+    clean_backups(prefix)
+
+def clean_backups(prefix):
+    """Keep just one backup per month and 10 at max."""
+    backups = glob(os.path.join(tw.sqldir, 'backups', prefix+'_*.sql'))
+    backups.sort(reverse=True)
+    current_month = None
+    for backup in backups.copy():
+        month = backup[-14:-7]
+        if month == current_month:
+            backups.remove(backup)
+            os.unlink(backup)
+        current_month = month
+    for backup in backups[10:]:
+        os.unlink(backup)
 
 
 backup('gores', ['race', 'teamrace'])
